a1eed79fbad08d6dbeae191cc82bd35f64931b18
Néhémie Mubenga Maweja <132246535+El-Nehmo@users.noreply.github.com>
2025-06-02 19:59:47 +0200

    update routing path (mediaCard)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { SearchComponent } from './components/search/search.component';
 import { HomeComponent } from './components/home/home.component';
+import { MediaDetailComponent } from './components/media-detail/media-detail.component';
 
 export const routes: Routes = [
 
     { path: '', component: HomeComponent},
-    { path: 'search', component: SearchComponent}
+    { path: 'search', component: SearchComponent},
+    { path: 'media/:id', component: MediaDetailComponent },
 
 ];
