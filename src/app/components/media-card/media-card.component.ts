49bfbaf4c142ea8fdb6b7c486ba6386954573ed9
Néhémie Mubenga Maweja <132246535+El-Nehmo@users.noreply.github.com>
2025-06-02 20:18:48 +0200

    added the function voirDetails()

--- src/app/components/media-card/media-card.component.ts
+++ src/app/components/media-card/media-card.component.ts
@@ -1,13 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { Media } from '../../models/media.model';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-media-card',
-  imports: [],
   templateUrl: './media-card.component.html',
-  styleUrl: './media-card.component.css'
+  styleUrls: ['./media-card.component.css']
 })
 export class MediaCardComponent {
-  @Input() media!: Media
+  @Input() media!: Media;
+
+  constructor(private router: Router) {}
+
+  voirDetails() {
+    this.router.navigate(['/media', this.media.id_tmdb]);
+  }
 }
