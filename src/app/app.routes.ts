b0e966e5aa67591a825562e5c1d5d3c2c8778b75
Néhémie Mubenga Maweja <132246535+El-Nehmo@users.noreply.github.com>
2025-06-08 18:29:46 +0200

    ajout des routes watchlist login et register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,17 @@ import { Routes } from '@angular/router';
 import { SearchComponent } from './components/search/search.component';
 import { HomeComponent } from './components/home/home.component';
 import { MediaDetailComponent } from './components/media-detail/media-detail.component';
+import { WatchlistComponent } from './components/watchlist/watchlist.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
 
     { path: '', component: HomeComponent},
     { path: 'search', component: SearchComponent},
     { path: 'media/:id/:type', component: MediaDetailComponent },
+    { path: 'watchlist', component: WatchlistComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
 
 ];
