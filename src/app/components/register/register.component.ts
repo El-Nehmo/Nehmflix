f7fb277b52bd4b0543f318c1f0f9f01788e32347
Néhémie Mubenga Maweja <132246535+El-Nehmo@users.noreply.github.com>
2025-06-10 17:31:21 +0200

    ajout du formulaire d'inscription

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,13 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule , FormGroup, FormControl, Validators} from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  registerForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      const { username, email, password } = this.registerForm.value;
+      console.log('Inscription avec :', username, email, password);
+      //API .NET plus tard ici
+    } else {
+      console.warn('Formulaire d’inscription invalide');
+    }
+  }
 
 }
