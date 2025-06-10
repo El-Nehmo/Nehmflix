9d028f188c09ccef6706ee622686698abf78ca6e
Néhémie Mubenga Maweja <132246535+El-Nehmo@users.noreply.github.com>
2025-06-10 17:01:13 +0200

    ajout du formulaire de connexion

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { routes } from './app.routes';
 import { SearchComponent } from './components/search/search.component';
 import { MediaDetailComponent } from './components/media-detail/media-detail.component';
@@ -9,11 +9,13 @@ import { WatchlistComponent } from './components/watchlist/watchlist.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     importProvidersFrom(FormsModule),
+    importProvidersFrom(ReactiveFormsModule),
     provideHttpClient(),
     SearchComponent,
     MediaDetailComponent,
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      console.log('Connexion avec :', email, password);
+    } else {
+      console.warn('Formulaire invalide');
+    }
+  }
 
 }
